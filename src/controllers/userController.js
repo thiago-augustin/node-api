359f3e4f5393657ccf78c98d86984c85456ff826
thiago-augustin <75146536+thiago-augustin@users.noreply.github.com>
2022-06-24 14:50:46 -0300

    Adicionando função de atualizar Usuário

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -32,6 +32,18 @@ class UserController {
       }
     })
   }
+
+  static updateUser = (req, res) => {
+    const id = req.params.id;
+
+    users.findByIdAndUpdate(id, {$set: req.body}, (err) => {
+      if(!err) {
+        res.status(200).send({message: 'User updated successfully'})
+      } else {
+        res.status(500).send({message: err.message})
+      }
+    })
+  }
 }
 
 export default UserController
