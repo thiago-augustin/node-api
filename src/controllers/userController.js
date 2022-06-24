e0a5ab18542f6ca0e73fac95d14c74fdd49d23d9
thiago-augustin <75146536+thiago-augustin@users.noreply.github.com>
2022-06-24 14:45:45 -0300

    Adicionando função de listar Usuários por ID

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -7,6 +7,18 @@ class UserController {
       res.status(200).json(users)
   })
   }
+
+  static listUserId = (req, res) => {
+    const id = req.params.id;
+
+    autores.findById(id, (err, users) => {
+      if(err) {
+        res.status(404).send({message: `${err.message} - User not found.`})
+      } else {
+        res.status(200).send(users);
+      }
+    })
+  }
 }
 
 export default UserController
