d229fc1c3bcb34c89ccc9adaf0581c417fd3d16a
Cidrallll <lucas.woginiack@yahoo.com.br>
2022-06-26 14:01:45 -0300

    Add taskRoutes

---
+++ src/routes/taskRoutes.js
@@ -0,0 +1,13 @@
+import express from "express";
+import TaskController from "../controllers/taskController.js";
+
+const router = express.Router();
+
+router
+  .get("/api/v1/tasks", TaskController.listTasks)
+  .get("/api/v1/tasks/:id", TaskController.listTaskId)
+  .post("/api/v1/tasks", TaskController.createTask)
+  .put("/api/v1/tasks/:id", TaskController.updateTask)
+  .delete("/api/v1/tasks/:id", TaskController.deleteTask)
+
+export default router;   
